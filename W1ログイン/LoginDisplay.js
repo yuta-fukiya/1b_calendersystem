b710ab38a66659fdfc92da481bb21b0bc6737e62
yuta-fukiya <f.yuta825@outlook.com>
2022-05-31 13:37:57 +0900

    ログイン

---
+++ W1ログイン/LoginDisplay.js
@@ -0,0 +1,19 @@
+const trueID="aaa";
+const truePassword="123";
+LoginID_value=null;
+Password_value=null;
+function IDSet(){
+    LoginID_value = document.getElementById("LoginText").value;
+}
+function PasswordSet(){
+    Password_value = document.getElementById("PasswordText").value;
+}
+function PushDecide(){
+    if(LoginID_value==trueID && Password_value==truePassword){
+        window.location.href = '../W2メイン/MainDisplay.html';
+    }else if(LoginID_value==null || Password_value==null){
+        alert("入力されていない欄があります．");
+    }else{
+        alert("ログインIDまたはパスワードが違います．");
+    }
+}
