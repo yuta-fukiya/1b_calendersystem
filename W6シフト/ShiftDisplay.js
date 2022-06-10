8ea38624374f410b972bcbf34bec2000199b6324
yuta-fukiya <f.yuta825@outlook.com>
2022-06-10 15:42:37 +0900

    シフト

--- W6シフト/ShiftDisplay.js
+++ W6シフト/ShiftDisplay.js
@@ -1,27 +1,45 @@
-var target = "";
-function MethodLink(){
-	var url = document.method.select.options[document.method.select.selectedIndex].value;
-	if(url != "" ){
-		if(target == 'top'){
-			top.location.href = url;
-		}
-		else if(target == 'blank'){
-			window.open(url);
-		}
-		else if(target != ""){
-			eval('parent.' + target + '.location.href = url');
-		}
-		else{
-			location.href = url;
-		}
+/*
+import AskJob from MainJobs;
+import UpdateJob from MainJobs;
 
-	}
+/********************************* 
+***function name  :ShiftData_update
+***Designer       :吹谷　優太
+***Date           :
+***Function       :シフト設定処理部へ入力されたデータを送る
+***********************************/
+
+
+function ShiftData_update(){
+   var shiftdata = [];
+
+    shiftdata.push = document.getElementById("JobName").value;                           //アルバイト名
+    shiftdata.push = document.getElementById("HourWages").value;                       //時給
+    shiftdata.push = document.getElementById("TrasCosts").value;               //交通費
+    shiftdata.push = document.getElementById("NightWages").value;             //深夜給
+    shiftdata.push = document.getElementById("Overtime").value;                //残業代
+    shiftdata.push = document.getElementById("NightWages_time_s").value;    //深夜給になる時間帯の始まり
+    shiftdata.push = document.getElementById("NightWages_time_f").value;    //深夜給になる時間帯の終わり
+
+    UpdateJobs(shiftdata, "Shift_UI");
+}
+
+/********************************* 
+***function name  :ShiftData_init
+***Designer       :吹谷　優太
+***Date           :
+***Function       :シフト設定処理部へデータを受け取り画面に反映する
+***********************************/
+function ShiftData_init(){
+   //var shiftdata = AskJobs("Shift_UI");
+
+   document.getElementById("JobName").placeholder =  "焼肉屋さん";   //shiftdata[0];   //アルバイト名
+   document.getElementById("HourWages").placeholder = "1000"        //shiftdata[1];   //時給
+   document.getElementById("TrasCosts").placeholder = "1000"        //shiftdata[2];   //交通費
+   document.getElementById("NightWages").placeholder = "1000"       //shiftdata[3];   //深夜給
+   document.getElementById("Overtime").placeholder = "1000"         //shiftdata[4];   //残業代
+   document.getElementById("NightWages_time_s").value = "12:12"     //shiftdata[5];   //深夜給になる時間帯の始まり
+   document.getElementById("NightWages_time_f").value = "21:00"     //shiftdata[6];   //深夜給になる時間帯の終わり
 
 }
-function modal(num){
-	if (num == "1")
-		window.open("../W7週別シフト/WeekDisplay.html", null ,'width=800,height=350');
-	else if (num == "2")
-		window.open("../W8月別シフト/MonthDisplay.html", null ,'width=600,height=600');
-}
-modal(NUM);
+window.onload = ShiftData_init;
