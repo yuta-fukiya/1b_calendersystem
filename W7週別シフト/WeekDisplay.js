b358732a80f645cedf0fffd59e5715feef2aa9b6
yuta-fukiya <f.yuta825@outlook.com>
2022-06-10 15:43:56 +0900

    週別シフト

--- W7週別シフト/WeekDisplay.js
+++ W7週別シフト/WeekDisplay.js
@@ -1,27 +1,57 @@
-const weeks = ['日','月','火','水','木','金','土'];
+/*import AskJobs from MainJob;
+  import UpdateJobs from MainJob;
+/*****************************************************************
+***function name     :Week_DataSave
+***Designer          :吹谷　優太
+***Date              :
+***function          :現在カレンダーに反映しているシフト時間を反映する
+ *******************************************************************/
 
-let dayCount = 1;   //日にちのカウント
-let calenderHtml = '';  // HTMLを組み立てる変数
+function Week_DataInit(){
+ //   var weekshift = AskJobs("WeekShift_UI");
 
-//冒頭の年月を表示
-calenderHtml += '<table>';
-
-//曜日の行を作成
-for (let i =0; i < weeks.length; i++){
-  calenderHtml += '<td>' + weeks[i] + '</td>';
+    document.getElementById("weekshift_sun_s").value = "12:00"; //weekshift[0]; 
+    document.getElementById("weekshift_sun_f").value = "12:00"; //weekshift[1];
+    document.getElementById("weekshift_mon_s").value = "12:00"; //weekshift[2];
+    document.getElementById("weekshift_mon_f").value = "12:00"; //weekshift[3];
+    document.getElementById("weekshift_tue_s").value = "12:00"; //weekshift[4];
+    document.getElementById("weekshift_tue_f").value = "12:00"; //weekshift[5];
+    document.getElementById("weekshift_wed_s").value = "12:00"; //weekshift[6];
+    document.getElementById("weekshift_wed_f").value = "12:00"; //weekshift[7];
+    document.getElementById("weekshift_thu_s").value = "12:00"; //weekshift[8];
+    document.getElementById("weekshift_thu_f").value = "12:00"; //weekshift[9];
+    document.getElementById("weekshift_fri_s").value = "12:00"; //weekshift[10];
+    document.getElementById("weekshift_fri_f").value = "12:00"; //weekshift[11];
+    document.getElementById("weekshift_sut_s").value = "12:00"; //weekshift[12];
+    document.getElementById("weekshift_sut_f").value = "12:00"; //weekshift[13];
 }
+window.onload = Week_DataInit;
+     
+/*******************************************
+***function name     :Week_DataSave
+***Designer          :吹谷　優太
+***Date              :
+***function          :シフト設定処理部に週別シフトのデータを送る
+ ********************************************/
 
-for (let w = 0; w < 1; w++) {
-  calenderHtml += '<tr>';
-  
-  for (let d = 0; d < 7; d++) {
-      calenderHtml += '<td>' + "時間" + '</td>'
-  }
-
-  calenderHtml += '</tr>';  
+function Week_DataSave(){
+    var weekshift = [];
+ 
+    weekshift.push = document.getElementById("weekshift_sun_s").value;
+    weekshift.push = document.getElementById("weekshift_sun_f").value;
+    weekshift.push = document.getElementById("weekshift_mon_s").value;
+    weekshift.push = document.getElementById("weekshift_mon_f").value;
+    weekshift.push = document.getElementById("weekshift_tue_s").value;
+    weekshift.push = document.getElementById("weekshift_tue_f").value;
+    weekshift.push = document.getElementById("weekshift_wed_s").value;
+    weekshift.push = document.getElementById("weekshift_wed_f").value;
+    weekshift.push = document.getElementById("weekshift_thu_s").value; 
+    weekshift.push = document.getElementById("weekshift_thu_f").value;
+    weekshift.push = document.getElementById("weekshift_fri_s").value;
+    weekshift.push = document.getElementById("weekshift_fri_f").value;
+    weekshift.push = document.getElementById("weekshift_sut_s").value;
+    weekshift.push = document.getElementById("weekshift_sut_f").value;
+ 
+    UpdateJobs(weekshift, "WeekShift_UI");
 }
-calenderHtml += '</table>';
-
-// HTMLに上記のコードを挿入
-document.querySelector('#calendar').innerHTML = calenderHtml;
-
+ 
