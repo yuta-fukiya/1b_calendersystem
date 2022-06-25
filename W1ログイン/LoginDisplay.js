e061ba678ac5a96e04fd1a27d67b286a29179159
yuta-fukiya <f.yuta825@outlook.com>
2022-06-25 20:23:10 +0900

    画面

--- W1ログイン/LoginDisplay.js
+++ W1ログイン/LoginDisplay.js
@@ -2,29 +2,34 @@ var xhr = new XMLHttpRequest();
 export function UserRegistration(){
     const id = document.getElementById("userID").value;
     const pass = document.getElementById("userPASS").value;
-    if (id != "" || pass != ""){
-    var result;
-    var data = [];
-    data.push("UserRegistration");
-    data.push(id);
-    data.push(pass);
-    xhr.open("POST", "/login.txt", true);
-    xhr.onreadystatechange = function (){
-        if(this.readyState === 4){
-            if(this.status === 200 || this.status == 0){
-                result = this.responseText;
-                if(result=="success"){
-                    alert("登録成功");
-                } else if (result == "already"){
-                    alert ("入力されたIDは使用することができません");
-                } else {
-                    alert("登録失敗");
+    if (id != "" && pass != ""){
+        if (id.match(/^[A-Za-z0-9]*$/) && pass.match(/^[A-Za-z0-9]*$/)){
+            var result;
+            var data = [];
+            data.push("UserRegistration");
+            data.push(id);
+            data.push(pass);
+            xhr.open("POST", "/login.txt", true);
+            xhr.onreadystatechange = function (){
+                if(this.readyState === 4){
+                    if(this.status === 200 || this.status == 0){
+                        result = this.responseText;
+                        if(result=="success"){
+                            alert("登録成功");
+                            window.history.back(-1);
+                        } else if (result == "already"){
+                            alert ("入力されたIDは使用することができません");
+                        } else {
+                            alert("登録失敗");
+                        }
+                    }
                 }
             }
+        } else {
+            alert("半角英数字で入力してください");
         }
-    }
-    xhr.send(data);
-    } else {
+        xhr.send(data);
+    } else{
         alert("入力していない項目があります");
     }
 }
@@ -46,10 +51,9 @@ export function AskRegistration(){
                 result = this.responseText;
                 console.log(result);
                 if(result=="success"){
-                    alert("ログイン成功");
                     window.location.href = './MainDisplay.html';
                 }else{
-                    alert("ログイン失敗");
+                    alert("IDまたはパスワードが違います");
                 }
                         
             }
