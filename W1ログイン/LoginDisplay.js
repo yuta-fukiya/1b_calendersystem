09da62d1e2516d6aa9805c91d088d7821a7782fd
yuta-fukiya <f.yuta825@outlook.com>
2022-06-25 16:22:57 +0900

    画面

--- W1ログイン/LoginDisplay.js
+++ W1ログイン/LoginDisplay.js
@@ -1,19 +1,59 @@
-const trueID="aaa";
-const truePassword="123";
-LoginID_value=null;
-Password_value=null;
-function IDSet(){
-    LoginID_value = document.getElementById("LoginText").value;
-}
-function PasswordSet(){
-    Password_value = document.getElementById("PasswordText").value;
-}
-function Certification(){
-    if(LoginID_value==trueID && Password_value==truePassword){
-        window.location.href = '../W2メイン/MainDisplay.html';
-    }else if(LoginID_value==null || Password_value==null){
-        alert("入力されていない欄があります．");
-    }else{
-        alert("ログインIDまたはパスワードが違います．");
+var xhr = new XMLHttpRequest();
+export function UserRegistration(){
+    const id = document.getElementById("userID").value;
+    const pass = document.getElementById("userPASS").value;
+    if (id != "" || pass != ""){
+    var result;
+    var data = [];
+    data.push("UserRegistration");
+    data.push(id);
+    data.push(pass);
+    xhr.open("POST", "/login.txt", true);
+    xhr.onreadystatechange = function (){
+        if(this.readyState === 4){
+            if(this.status === 200 || this.status == 0){
+                result = this.responseText;
+                if(result=="success"){
+                    alert("登録成功");
+                } else if (result == "already"){
+                    alert ("入力されたIDは使用することができません");
+                } else {
+                    alert("登録失敗");
+                }
+            }
+        }
+    }
+    xhr.send(data);
+    } else {
+        alert("入力していない項目があります");
     }
 }
+window.UserRegistration=UserRegistration;
+    
+export function AskRegistration(){
+    const id = document.getElementById("userID").value;
+    const pass = document.getElementById("userPASS").value;
+    var result;
+    var data = [];
+    data.push("AskRegistration");
+    data.push(id);
+    data.push(pass);
+    xhr.open("POST", "./login.txt", true);
+    xhr.send(data);
+    xhr.onreadystatechange = function (){
+        if(this.readyState === 4){
+            if(this.status === 200 || this.status == 0){
+                result = this.responseText;
+                console.log(result);
+                if(result=="success"){
+                    alert("ログイン成功");
+                    window.location.href = './MainDisplay.html';
+                }else{
+                    alert("ログイン失敗");
+                }
+                        
+            }
+        }
+    }
+}
+window.AskRegistration=AskRegistration;
