41b0f474754cd3ea605e41284de22e03dab179ae
Kent1524 <kent0524kino@gmail.com>
2022-07-01 17:24:12 +0900

    W4,5修正

---
+++ W5時間割詳細/timetabledetail.js
@@ -0,0 +1,30 @@
+function TimeTableDetails_init(){
+    var timetabledata = AskTimetableJob();
+    document.getElementById("Class").paceholder = timetabledata[0];
+    document.getElementById("NumClasses").paceholder = timetabledata[1];
+    document.getElementById("Unit").paceholder = timetabledata[2];
+    document.getElementById("Professor").paceholder = timetabledata[3];
+}
+window.onload = TimeTableDetails_init;
+
+export function TimeTableDetails_update(){
+    var timetabledata = [];
+    timetabledata.push(document.getElementById("Class").value);
+    if(timetabledata[0] == ""){
+        timetabledata[0] = document.getElementById("Class").ariaPlaceholder;
+    }
+    timetabledata.push(document.getElementById("NumClasses").value);
+    if(timetabledata[1] == ""){
+        timetabledata[1] = document.getElementById("NumClasses").ariaPlaceholder;
+    }
+    timetabledata.push(document.getElementById("Unit").value);
+    if(timetabledata[2] == ""){
+        timetabledata[2] = document.getElementById("Unit").ariaPlaceholder;
+    }
+    timetabledata.push(document.getElementById("Professor").value);
+    if(timetabledata[3] == ""){
+        timetabledata[3] = document.getElementById("Professor").ariaPlaceholder;
+    }
+}
+
+window.TimeTableDetails_update=TimeTableDetails_update;
