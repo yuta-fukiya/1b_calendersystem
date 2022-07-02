24a2a7c3d1656429b0efe8f69515148382133c9c
yuta-fukiya <f.yuta825@outlook.com>
2022-07-03 03:13:45 +0900

    処理部に分割

--- W1ログイン/LoginDisplay.js
+++ W1ログイン/LoginDisplay.js
@@ -1,63 +1,38 @@
-var xhr = new XMLHttpRequest();
+import {CheckUser, RegistUser} from "./MainLogin.js";
+
 export function UserRegistration(){
     const id = document.getElementById("userID").value;
     const pass = document.getElementById("userPASS").value;
     if (id != "" && pass != ""){
         if (id.match(/^[A-Za-z0-9]*$/) && pass.match(/^[A-Za-z0-9]*$/)){
-            var result;
-            var data = [];
-            data.push("UserRegistration");
-            data.push(id);
-            data.push(pass);
-            xhr.open("POST", "/login.txt", true);
-            xhr.onreadystatechange = function (){
-                if(this.readyState === 4){
-                    if(this.status === 200 || this.status == 0){
-                        result = this.responseText;
-                        if(result=="success"){
-                            alert("登録成功");
-                            window.history.back(-1);
-                        } else if (result == "already"){
-                            alert ("入力されたIDは使用することができません");
-                        } else {
-                            alert("登録失敗");
-                        }    
-                    }
-                }
-            }
+            var result = RegistUser(id, pass);
+            if(result=="success"){
+                alert("登録成功");
+                window.history.back(-1);
+            } else if (result == "already"){
+                alert("入力されたIDは使用することができません");
+            } else {
+                alert("登録失敗");
+            }    
         } else {
             alert("半角英数字で入力してください");
         }
-        xhr.send(data);
     } else{
         alert("入力していない項目があります");
     }
 }
 window.UserRegistration=UserRegistration;
     
-export function AskRegistration(){
+export async function AskRegistration(){
     const id = document.getElementById("userID").value;
     const pass = document.getElementById("userPASS").value;
-    var result;
-    var data = [];
-    data.push("AskRegistration");
-    data.push(id);
-    data.push(pass);
-    xhr.open("POST", "./login.txt", true);
-    xhr.send(data);
-    xhr.onreadystatechange = function (){
-        if(this.readyState === 4){
-            if(this.status === 200 || this.status == 0){
-                result = this.responseText;
-                console.log(result);
-                if(result=="success"){
-                    window.location.href = './MainDisplay.html?'+id;
-                }else{
-                    alert("IDまたはパスワードが違います");
-                }
-                        
-            }
-        }
+    var result = CheckUser(id, pass);
+    console.log(result);
+    if(result=="success"){
+        window.location.href = './MainDisplay.html?'+id;
+    }else{
+        console.log(result);
+        alert("IDまたはパスワードが違います");
     }
 }
 window.AskRegistration=AskRegistration;
