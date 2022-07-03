78f1b9ef08df3a8e096a3fd73bd6f3681ac8ea61
yuta-fukiya <f.yuta825@outlook.com>
2022-07-04 02:34:53 +0900

    コメントアウト追加

--- C2認証処理部/MainLogin.js
+++ C2認証処理部/MainLogin.js
@@ -1,8 +1,14 @@
 var xhr = new XMLHttpRequest();
 
+/*****************************************************************
+***function name     :CheckUser
+***Designer          :吹谷　優太
+***Date              :
+***function          :ユーザ情報の参照結果をザーバから受け取る
+ *******************************************************************/
 export function CheckUser(id, pass){
-    var result;
-    var data = [];
+    var result;                     //参照結果を代入する変数
+    var data = [];                  //サーバに送るデータの配列
     data.push("AskRegistration");
     data.push(id);
     data.push(pass);
@@ -14,9 +20,15 @@ export function CheckUser(id, pass){
     return result;
 }
 
+/*****************************************************************
+***function name     :RegistUser
+***Designer          :吹谷　優太
+***Date              :
+***function          :新規ユーザ情報をサーバに送信する
+ *******************************************************************/
 export function RegistUser(id, pass){
-    var result;
-    var data = [];
+    var result;                      //登録結果を代入する変数
+    var data = [];                   //サーバに送るデータの配列
     data.push("UserRegistration");
     data.push(id);
     data.push(pass);
