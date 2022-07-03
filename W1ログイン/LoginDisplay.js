56bef2196222375064e3764e72fa131171cbf842
yuta-fukiya <f.yuta825@outlook.com>
2022-07-04 02:36:44 +0900

    コメントアウト追加

--- W1ログイン/LoginDisplay.js
+++ W1ログイン/LoginDisplay.js
@@ -1,11 +1,17 @@
 import {CheckUser, RegistUser} from "./MainLogin.js";
 
+/*****************************************************************
+***function name     :UserResistration
+***Designer          :吹谷　優太
+***Date              :
+***function          :新規ユーザ情報を認証処理部にに送信する
+ *******************************************************************/
 export function UserRegistration(){
-    const id = document.getElementById("userID").value;
-    const pass = document.getElementById("userPASS").value;
-    if (id != "" && pass != ""){
-        if (id.match(/^[A-Za-z0-9]*$/) && pass.match(/^[A-Za-z0-9]*$/)){
-            var result = RegistUser(id, pass);
+    const id = document.getElementById("userID").value;               //入力したID
+    const pass = document.getElementById("userPASS").value;           //入力したパスワード
+    if (id != "" && pass != ""){                                          //未入力判定
+        if (id.match(/^[A-Za-z0-9]*$/) && pass.match(/^[A-Za-z0-9]*$/)){  //半角英数字判定
+            var result = RegistUser(id, pass);                        //登録結果を代入した変数
             if(result=="success"){
                 alert("登録成功");
                 window.history.back(-1);
@@ -22,11 +28,17 @@ export function UserRegistration(){
     }
 }
 window.UserRegistration=UserRegistration;
-    
+
+/*****************************************************************
+***function name     :AskResistration
+***Designer          :吹谷　優太
+***Date              :
+***function          :認証処理部から認証結果を受け取る
+ *******************************************************************/
 export async function AskRegistration(){
-    const id = document.getElementById("userID").value;
-    const pass = document.getElementById("userPASS").value;
-    var result = CheckUser(id, pass);
+    const id = document.getElementById("userID").value;              //入力したID
+    const pass = document.getElementById("userPASS").value;          //入力したパスワード
+    var result = CheckUser(id, pass);                                //認証結果を代入した変数
     console.log(result);
     if(result=="success"){
         window.location.href = './MainDisplay.html?'+id;
