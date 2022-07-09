04cd39132f9c7c5a0831c1a41e822c0182e034e5
Daiki Inokoshi <al20116@shibaura-it.ac.jp>
2022-07-10 00:28:54 +0900

    月別シフト更新

--- W8月別シフト/MonthDisplay.js
+++ W8月別シフト/MonthDisplay.js
@@ -3,6 +3,7 @@ const today = new Date();
 var showDate = new Date(today.getFullYear(), today.getMonth(), 1);
 
 window.onload = function () {
+    createId();
     showProcess(today, calendar);
 };
 
@@ -48,22 +49,51 @@ function createProcess(year, month) {
                 calendar += "<td class='disabled'>" + (count - endDate) + "</td>";
             } else {
                 count++;
-                calendar += "<td onclick='clickBtn()'>" + count + "</td>";
+                calendar += "<td onclick='showProcess2(" + year + "," + (month+1) + "," + count +")'>" + count + "</td>"
             }
         }
         calendar += "</tr>";
     }
     return calendar;
 }
-
+/*
 function clickBtn(){
     const start = prompt("開始時刻","9:00");
     if(start==null) return;
     const end = prompt("終了時刻","18:00");
     if(end==null) return;
 
+}*/
+
+function showProcess2(year, month, day) {
+    document.getElementById("Shift").classList.add("Shift-after");
+    var calender2 = createProcess2(year, month, day);
+    document.querySelector('#Shift').innerHTML = calender2;
+}
+
+function createProcess2(year, month, day) {
+    var startId = "monthshift_" + day + "_s";
+    var endId = "monthshift_" + day + "_f"
+    var calender2 = "<h2>" + month + "月 "+ day + "日"+ "</h2>";
+    calender2 += "<tr><br>開始時刻" + "<input type='time' id=" + startId + ">";
+    calender2 += "<br><br>終了時刻" + "<input type='time' id="+ endId+ ">";
+    calender2 += "</tr>";
+    return calender2;
 }
 
 function back(){
-    window.close();
+    alert(document.getElementById("monthshift_25_s").value);
+    //window.close();
+}
+
+function createId() {
+    var calender3 = "";
+    alert("check1");
+    for (let i=1; i<=31; ++i) {
+        var temp1 = "monthshift_" + i + "_s";
+        var temp2 = "monthshift_" + i + "_f";
+        calender3 += "<div id=" + temp1 + "></div>"
+        calender3 += "<div id=" + temp2 + "></div>"
+    }
+    document.querySelector('#shiftmonth').innerHTML = calender3;
 }
